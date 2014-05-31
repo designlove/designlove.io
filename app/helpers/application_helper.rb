a9b10544383ee0576d4a4bce23b4c23e35b6158f
DeVaris Brown <devaris@devaris.com>
2014-05-31 17:22:59 +0100

    css fixes for masthead and about section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def typekit_include_tag(apikey)
+    javascript_include_tag("https://use.typekit.com/#{apikey}.js") +
+    javascript_tag("try{Typekit.load()}catch(e){}")
+  end
 end
